f0cc9eb98d81773e433ce05ed48db4b397134b27
Ashish Shakya <ashishakya@outlook.com>
2018-03-03 18:11:13 +0545

    Update 3_first.py

--- 3_first.py
+++ 3_first.py
@@ -6,7 +6,7 @@ cars_not_driven = cars - drivers
 cars_driven = drivers
 carpool_capacity = cars_driven * space_in_a_car
 average_passenger_per_car = passengers / cars_driven
-
+// this is the comment section
 print("There are " +  str(cars) + "  cars available") #first method
 
 print("There are" , cars , "cars available" ) #Secong method
